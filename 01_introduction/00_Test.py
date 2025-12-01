2264b8b2682ee3e81a56c96837d1ac268d7d9bfe
Adal <adalsuuygur@gmail.com>
2025-12-01 20:44:27 +0300

    ongoing lesson for satır sütun

--- 01_introduction/00_Test.py
+++ 01_introduction/00_Test.py
@@ -11,20 +11,20 @@ print(lst_1)
 lst_2 = [i for i in range(10)]
 print(lst_2)
 
-#rastgele üretilmiş 10 sayı ile list comprehension uygulaması
+#todo rastgele üretilmiş 10 sayı ile list comprehension uygulaması
 from random import randint
 lst_random = [randint(a=10, b=100) for _ in range(10)]
 print(lst_random)
 
-#rakamların karesinin list compehension ile oluşturulması
+#todo rakamların karesinin list compehension ile oluşturulması
 lst_3 = [i**2 for i in range(10)]
 print(lst_3)
 
-#0-100 arasındaki çift sayıları listeye eklenmesi
+#todo 0-100 arasındaki çift sayıları listeye eklenmesi
 lst_4 = [i for i in range(101) if i%2 == 0] #0 ile 100 dahil olsun diye
 print(lst_4)
 
-#içerisinde a harfi geçen meyvelerin listeye alınması
+#todo içerisinde a harfi geçen meyvelerin listeye alınması
 fruits = [
     "Apple", "Banana", "Orange", "Mango", "Pineapple",
     "Strawberry", "Grapes", "Watermelon", "Peach", "Cherry",
@@ -34,3 +34,17 @@ fruits = [
 
 lst_5 = [fruit for fruit in fruits if "a" in fruit.lower()]
 print(lst_5)
+
+#todo ternary if ile, meyve içerisinde "an" geçiyorsa true, geçmiyorsa false eklenen list comprehension
+lst_6 = [True if "an" in fruit.lower() else False for fruit in fruits]
+print(lst_6)
+
+#todo çarpım tablosu, satır-sütun algoritması
+#* SATIR SÜTUN ALGORİTMALARINDA: i satırı, j sütunu yönetir, yani i 1. adımındayken j orada belirtilen adım kadar çalışır.
+
+for i in range(1,11):
+    for j in range(1,11):
+        print(f"{i} x {j} = {i * j}")
+    print("-------------------")
+
+
