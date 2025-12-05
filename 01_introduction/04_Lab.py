90175ca1e44ca26c18bac404e0e97451c2f90e09
Adal <adalsuuygur@gmail.com>
2025-12-05 03:20:57 +0300

    examples added

--- 01_introduction/04_Lab.py
+++ 01_introduction/04_Lab.py
@@ -36,6 +36,65 @@ except TypeError as err:
 
 #region Examples
 
+#todo Login, 3 yanlış hak, try-except & match-case & infinitive loop kullanımı isteniliyor. Login yapıldığında hesap makinesine ulaşılacak.
+username = "adal"
+password = "123"
+#isim ve şifre belirledik
+hak = 3
+while hak > 0:
+#kullanıcıdan da istedik neden döngü içinde, her seferinde sorabilmek için
+    user_name = input("Username: ")
+    pass_word = input("Password: ")
+    if user_name == username and pass_word == password:
+        print("Login successful") #login olduk ama hak için bir şey yapmadık. Test lazım.
+        while True:
+            try:
+                n1 = int(input("Number1: "))
+                n2 = int(input("Number2: "))
+                operation = input("Enter desired matematical operation, e for exit: ")
+                match operation:
+                    case "+":
+                        result = n1 + n2
+                        print(f"Your calculation is : {n1} {operation} {n2} = {result}") #DRY prensibine uyamadım.
+                    case "-":
+                        result = n1 - n2
+                        print(f"Your calculation is : {n1} {operation} {n2} = {result}")
+                    case "*":
+                        result = n1 * n2
+                        print(f"Your calculation is : {n1} {operation} {n2} = {result}")
+                    case "/":
+                        result = n1 / n2
+                        print(f"Your calculation is : {n1} {operation} {n2} = {result}")
+                    case "e":
+                        print("Exiting the program.")
+                        break
+                    case _:
+                        print("Invalid entry, try again.")
+            except (ValueError, TypeError, ZeroDivisionError) as err:
+                print(f"Cannot continue because of ({err}), try again.")
+    else:
+        print(f"Login denied!, remaining tries = {hak - 1}")
+        hak -= 1 #döngüye sokacağız mecbur, burda da hakkımızı düşürdük.
+print("Hakkınızı yitirdiniz, bye!") #While döngüsünden çıktığımızda hakkımız kalmamış olcak.
 
+#todo Girilen sayının asal olup olmadığını kontrol eden uygulama
+while True:
+    try:
+        sayi = int(input("Lütfen bir sayı giriniz: "))
+        is_asal = True
+        if sayi < 2: #2den küçük asal sayı yoktur ve negatif sayılar asal olamaz.
+            print("Negatif sayılarda asallık bakılmaz ve 2'den küçük sayılar asal değildir.")
+            break
+        else:
+            for i in range(2, sayi):
+                if sayi % i == 0:
+                    is_asal = False
+                    break
+            if is_asal == True:
+                print(f"Girilen sayı ({sayi}) asaldır.")
+            else:
+                print(f"Girilen sayı ({sayi}) asal değildir.")
+    except (ValueError, TypeError) as err:
+        print(f"{err} sebebiyle hata verildi, lütfen sayıyı tekrar giriniz.")
 
 #endregion
