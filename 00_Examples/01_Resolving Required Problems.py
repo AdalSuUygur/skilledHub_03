a8ba6774e24ace18d652e3cad797e9a433ca550d
Adal <adalsuuygur@gmail.com>
2025-12-09 21:21:43 +0300

    updated fixed problems

--- 00_Examples/01_Resolving Required Problems.py
+++ 00_Examples/01_Resolving Required Problems.py
@@ -1,65 +1,4 @@
 
-# #! small projects içindeki benchmarkı tekrar çöz, anla, gerekirse diğer projelerede benchmark ekle
-
-# #14. ders ödevi:
-# # 100.000 tane rastgele sayı üretilecek. List comprehension ile yapılacak
-# #path i, ii, iii t,öe ve öeöpry cost hesaplanarak ekrana rapor olarak yazdırın
-# #path i: list comprehensions ile pozitif sayılar bulunacak ve liste olarak ekrana basılacak
-# #path ii: filter onksiyonu ile pozitif...
-# #pathiii for loop ile
-
-# # #14. ders ödevine ek olarak: bu satır sutün algoritması için de time ve memory cost hesapla.
-# # #region Satır Sutün Algoritması
-# # #? Satır-Sütun algoritması
-# # #* SATIR SÜTUN ALGORİTMALARINDA: i satırı, j sütunu yönetir, yani i 1. adımındayken j orada belirtilen adım kadar çalışır.
-
-# # #Geleneksel yöntem:
-# # for i in range(1,11):
-# #     for j in range(1,11):
-# #         print(f"{i} x {j} = {i * j}")
-# #     print("-------------------")
-
-# # #List comprehension ile yapılması:
-# # lst_carpim_tablosu = [ 
-# #     (i*j) #yazdırılacak sonuç 
-# #     for i in range(1,11) #en dış döngü
-# #     for j in range(1, 11) #en iç döngü
-# # ]
-# # print(lst_carpim_tablosu)
-# # #* Tek satır olmadı yine alt satıra geçtik, ee ne anlamı kaldı list comprehension'ın? Go for benchmark testing!
-
-# # #List comprehension ile PRINT FONKSİYONU İÇİNDE yapılması:
-# # print(
-# #     [
-# #         [f"{i} x {j} = {i * j}" for i in range(1, 11)] for j in range(1, 11) #liste içinde liste yaptık
-# #     ]
-# #     ) #Burda aslında list içinde for içinde for ve liste içinde liste de tanımlıyoruz, sınırımız yok.
-# # #endregion
-
-# # #Geleneksel yöntem:
-# # for i in range(1,11):
-# #     for j in range(1,11):
-# #         print(f"{i} x {j} = {i * j}")
-# #     print("-------------------")
-
-# # #List comprehension ile yapılması:
-# # lst_carpim_tablosu = [ 
-# #     (i*j) #yazdırılacak sonuç 
-# #     for i in range(1,11) #en dış döngü
-# #     for j in range(1, 11) #en iç döngü
-# # ]
-# # print(lst_carpim_tablosu)
-# # #* Tek satır olmadı yine alt satıra geçtik, ee ne anlamı kaldı list comprehension'ın? Go for benchmark testing!
-
-# # #List comprehension ile PRINT FONKSİYONU İÇİNDE yapılması:
-# # print(
-# #     [
-# #         [f"{i} x {j} = {i * j}" for i in range(1, 11)] for j in range(1, 11) #liste içinde liste yaptık
-# #     ]
-# #     ) #Burda aslında list içinde for içinde for ve liste içinde liste de tanımlıyoruz, sınırımız yok.
-# # #endregion
-
-
 #todo Doğru loginde, ürün search ve fiyat output. Yanlış loginde yeni kayıt. Username'ler unique olacak.
 
 #bütün ürünlerin toplam fiyatı nedir
@@ -125,9 +64,3 @@ products = [
 #         )
 #     )
 # )
-
-
-
-
-
-
