9ff65ad8079a1c7862be62e05e0be3ea997b82c5
Adal <adalsuuygur@gmail.com>
2025-12-10 05:18:46 +0300

    worked on resolved required example

--- 00_Examples/01_Resolving Required Problems.py
+++ 00_Examples/01_Resolving Required Problems.py
@@ -1,14 +1,6 @@
 
-
-
-
-
-
-
-
-
-
-
+#hatalarım
+# u_plus_p = list(zip(*(username for username in users))) #bu yapı bana usernames listesi ve şifreler listesi verdi, ben burdan usernames çekmeye çalışıyorum
 
 #todo Doğru loginde, ürün search ve fiyat output. Yanlış loginde yeni kayıt. Username'ler unique olacak.
 
@@ -16,42 +8,60 @@
 #ürün adı laptop olan ürünlerin fiyatlarını toplayalım
 #kullanıcı ürün search edebilsin, yani ürün search etti monitör yazdı varsa fiyatını verdi
 #fiyatı 200 tl altında olan ürünler listelensin
-# #register olsun, yani kayıt da olsun; username varsa hali hazırda ekleyemesin
+#register olsun, yani kayıt da olsun; username varsa hali hazırda ekleyemesin
 
-# users = [
-#     ["beast", "123"],
-#     ["bear", "456"],
-#     ["keko", "789"]
-# ]
+users = [
+    ["beast", "123"],
+    ["bear", "456"],
+    ["keko", "789"]
+]
 
-# is_login = False
-# username = input("Username: ").lower()
-# password = input("Password: ")
-# for USERNAME, PASSWORD in users:
-#     if username == USERNAME and password == PASSWORD:
-#         is_login = True
-#         break
+def is_login(user_name, pass_word):
+    for USERNAME, PASSWORD in users:
+        if user_name == USERNAME and pass_word == PASSWORD:
+            return True
+    return False
+
+username = input("Username: ").lower()
+password = input("Password: ")
+case_login = is_login(username, password)
+
+while not case_login:
+    sign_in = input("Wrong entry, would you like to sign up(1) or try to sign in again?(0): ")
+    match sign_in:
+        case "1":
+            #* case1 içini çalıştıramadım, şu an giriş alsa bile eklemiyor sanırım listeye, debug gerek
+            # new_username = input("Please enter a username: ").lower()
+            # usernames = list(zip(*users))[0]
+            # if new_username not in usernames:
+            #     new_password = input("Please enter password: ")
+            #     users.append([new_username, new_password])
+            #     print("Welcome, you can login with new informations now.")
+            #     username = input("Username: ").lower()
+            #     password = input("Password: ")
+            #     new_case_login = is_login(username, password)
+            # else:
+            #     new_case_login = False
+            # if not new_case_login:
+            #     break
+#* case0 sorunsuz çalışıyor, kendime hayran kaldım :D
+        case "0":
+            username = input("Username: ").lower()
+            password = input("Password: ")
+            case_login = is_login(username, password)
+        case _:
+            print("Try again.")
 
 
-# while is_login == False:
 
-#     sign_in = input("Wrong entry, would you like to sign up(1) or try to sign in again?(0): ")
-#     match sign_in:
-#         case "1":
-#             new_username = input("Please enter a username: ").lower()
-#             if new_username not in USERNAME:
-#                 new_password = input("Please enter password: ")
-#                 users.append([new_username, new_password])
-#                 print("Welcome, you can login with new informations now.")
-#                 username = input("Username: ").lower()
-#                 password = input("Password: ")
-#                 for USERNAME, PASSWORD in users:
-#                     if username == USERNAME and password == PASSWORD:
-#                         is_login = True
-#         case "0":
-#             break
-#         case _:
-#             print("Try again.")
+
+            # if new_username not in USERNAME:
+
+            #     
+            #     print("Welcome, you can login with new informations now.")
+
+
+
 
 products = [
     ["Laptop", 850],
