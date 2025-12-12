f59561227bd0fb98f43496e89bc811d178e03190
Adal <adalsuuygur@gmail.com>
2025-12-12 03:47:28 +0300

    comment out

--- 00_Examples/01_Resolving Required Problems.py
+++ 00_Examples/01_Resolving Required Problems.py
@@ -10,47 +10,47 @@
 #fiyatı 200 tl altında olan ürünler listelensin
 #register olsun, yani kayıt da olsun; username varsa hali hazırda ekleyemesin
 
-users = [
-    ["beast", "123"],
-    ["bear", "456"],
-    ["keko", "789"]
-]
+# users = [
+#     ["beast", "123"],
+#     ["bear", "456"],
+#     ["keko", "789"]
+# ]
 
-def is_login(user_name, pass_word):
-    for USERNAME, PASSWORD in users:
-        if user_name == USERNAME and pass_word == PASSWORD:
-            return True
-    return False
+# def is_login(user_name, pass_word):
+#     for USERNAME, PASSWORD in users:
+#         if user_name == USERNAME and pass_word == PASSWORD:
+#             return True
+#     return False
 
-username = input("Username: ").lower()
-password = input("Password: ")
-case_login = is_login(username, password)
+# username = input("Username: ").lower()
+# password = input("Password: ")
+# case_login = is_login(username, password)
 
-while not case_login:
-    sign_in = input("Wrong entry, would you like to sign up(1) or try to sign in again?(0): ")
-    match sign_in:
-        case "1":
-            #* case1 içini çalıştıramadım, şu an giriş alsa bile eklemiyor sanırım listeye, debug gerek
-            # new_username = input("Please enter a username: ").lower()
-            # usernames = list(zip(*users))[0]
-            # if new_username not in usernames:
-            #     new_password = input("Please enter password: ")
-            #     users.append([new_username, new_password])
-            #     print("Welcome, you can login with new informations now.")
-            #     username = input("Username: ").lower()
-            #     password = input("Password: ")
-            #     new_case_login = is_login(username, password)
-            # else:
-            #     new_case_login = False
-            # if not new_case_login:
-            #     break
-#* case0 sorunsuz çalışıyor, kendime hayran kaldım :D
-        case "0":
-            username = input("Username: ").lower()
-            password = input("Password: ")
-            case_login = is_login(username, password)
-        case _:
-            print("Try again.")
+# while not case_login:
+#     sign_in = input("Wrong entry, would you like to sign up(1) or try to sign in again?(0): ")
+#     match sign_in:
+#         case "1":
+#             #* case1 içini çalıştıramadım, şu an giriş alsa bile eklemiyor sanırım listeye, debug gerek
+#             # new_username = input("Please enter a username: ").lower()
+#             # usernames = list(zip(*users))[0]
+#             # if new_username not in usernames:
+#             #     new_password = input("Please enter password: ")
+#             #     users.append([new_username, new_password])
+#             #     print("Welcome, you can login with new informations now.")
+#             #     username = input("Username: ").lower()
+#             #     password = input("Password: ")
+#             #     new_case_login = is_login(username, password)
+#             # else:
+#             #     new_case_login = False
+#             # if not new_case_login:
+#             #     break
+# #* case0 sorunsuz çalışıyor, kendime hayran kaldım :D
+#         case "0":
+#             username = input("Username: ").lower()
+#             password = input("Password: ")
+#             case_login = is_login(username, password)
+#         case _:
+#             print("Try again.")
 
 
 
