403ca1f9a047f70930adfaa1e8c918380e735ac0
Adal <adalsuuygur@gmail.com>
2025-12-24 01:38:50 +0300

    ders tekrarı updates

--- 01_introduction/11_Dictionary.py
+++ 01_introduction/11_Dictionary.py
@@ -61,3 +61,156 @@ release_year_movie.update( #update fonksiyonu bizden sözlük ister
 
 del release_year_movie["Dune II"]
 print(release_year_movie)
+
+#region Examples
+
+#todo Products listesindeki sözlüklerin içerisindeki işlemler
+products = [
+    {"name": "Lenovo X1 Carbon", "price": 110.000},
+    {"name": "Lenovo Thinkpad", "price": 89.000},
+    {"name": "Macbook Pro", "price": 250.000},
+    {"name": "Mackbook Air", "price": 125.000},
+    {"name": "Asus Zenbook", "price": 150.000},
+    {"name": "Monster Huma", "price": 55.000},
+    {"name": "Monster Alba"},
+    {"price": 250.000}
+]
+
+#* Products listesindeki tüm ürünlerin fiyatlarının toplamı:
+# Path I: Junior Çözümü
+# total_price = 0
+# for product in products:
+#     total_price += product.get("price")
+# print(total_price)
+
+# Path II: Mid Çözümü
+# total_price = sum(product.get("price") for product in products)
+# print(total_price)
+
+#* Products listesindeki ürünlerin fiyat aralığı 100.000'den büyük olanları listeleyelim
+# Path I: Kendi Çözümüm
+# print(list(
+#     product for product in products if product.get("price") > 100.000
+# ))
+
+# Path II: Hocanın Mid Seviye Çözümü
+# price_threshold = 100.000 #input olarak da kullanıcıdan alınabilir
+# filtered_product = [product for product in products if product.get("price") > price_threshold]
+# print(filtered_product)
+
+#* Ürün adı ("name") içerisinde "Lenovo" geçen ve fiyat aralığı 100.000-150.000 aralığında olan ürünleri listeleyin
+#* Burdaki çıkarım: .get() metodu içerisinde default tanımlama yapılması.
+# Path I: Kendi Çözümüm
+# print(list(
+#     product for product in products if ("Lenovo" in product.get("name")) and (100.000 < product.get("price") < 150.000)
+# )) #none type hatası geldi 2 eksik sözlük yapısını listeye ekleyince.
+
+# Path II: Hocanın çözümü
+# name_kwd = "Lenovo" #input olarak alabileceğimiz için hoca bunları bu şekilde değişkene atadı.
+# min_price = 100.000
+# max_price = 150.000
+
+# Benim çözümümle aynı, ne farkı var? Fark default tanımlama yaptı.
+# filtered_product = [
+#     product for product in products 
+#     if name_kwd in product.get("name", "") and  # Burada product.get("name", "") burada get metodu içine default değer tanımladık eğer tanımlanmamışsa diye
+#     #bu da name'i olmayan bir ürün geldiyse default değer olarak "" yazdır demek.
+#     min_price < product.get("price", 0) < max_price
+#     ]
+
+# print(filtered_product)
+
+#todo 19.ders yerine gelen ödev sorusu çözümü 
+# aşağıdaki categories sözlüğüne: Kullanıcı sınırsız işlem yapabilsin.
+# 1. yeni kayıt eklenecek (Create New Record)
+# 1.1. id bilgisi 'd912b8cf-0b59-4efb-bfcf-17356dd59c9b' uuid modülünden faydalanarak yaratılacak
+# 2. var olan bir kayıt güncellenecek (Update) --> kullanıcıdan id bilgisi alınacak ve ilgili kayıt güncelenecek
+# 3. var olan bir kayıt silinecek (Delete) --> kullanıcıdan id bilgisi alınacak ve ilgili kayıt güncelenecek
+# 4. Read operasyonları --> bütün kayıtlar listelenecek, kullanııdan ürün adı alınacak ve ilgli ürün listelenecek,
+
+categories = {
+    'd912b8cf-0b59-4efb-bfcf-17356dd59c9b': {
+        'name': 'Boxing Gloves',
+        'description': 'Best boxing gloves'
+    },
+    '9ecfa748-ee8e-4ac3-a471-33e1fd9fe52c': {
+        'name': 'MMA Gloves',
+        'description': 'Best MMA gloves'
+    }
+}
+
+from uuid import uuid4
+from pprint import pprint #pretty print demek bu, sözlükler için kullanıyoruz. Tatlış print :D
+
+print(uuid4()) #test
+print(type(uuid4)) #test
+
+# Soru şu: bizim UUID4 çıktısının tipi ile bizim elimizdeki dataların tipi farklı, elimizdeki data tipi string.
+# O zaman elimizdeki tipi de stringe çevirmemiz gerek.
+
+print(str(uuid4()))
+
+# while True:
+#     process = input("Type a process: ").lower() #kullanıcı bize yapacağı işi söylesin.
+# # value'ya atanmış olan idler key, karşılığında ise value olarak sözlük var.
+# # iç sözlükte 2 tane key var 2 tane de value var. 
+
+#     match process:
+#         case "create": #yapının aynısını kuruyoruz
+#             new_name = input("Please type a category name: ")
+#             new_descp = input("Please type a description name: ")
+#             categories[str(uuid4())] = {
+#                 "name": new_name,
+#                 "description": new_descp
+#             }
+# #birebir yukarıdaki yapıyı çalıştırdık
+#             pprint(categories)
+
+#         case "get all":
+#             pprint(categories)
+
+#         case "get by id":
+#             category_key = input("Category Key: ")
+#             result = categories.get(category_key)
+#             if result is None:
+#                 print("There is no category.")
+#             else:
+#                 print(result)
+
+#         case "get by name":
+#             # Kendi çözümüm:
+#             # categories.values() burda values olarak iç sözlüğe ulaştım
+#             # içerideki her değeri gezdim ve value değişkenindeki "name" adındaki keyi yazdırdım.
+#             # print(list(value["name"] for value in categories.values()))
+#             # Hocanın çözümü:
+#             cat_name = input("Category name: ")
+#             filtered_categories = [category for category in categories.values() if cat_name in category.get("name")]
+#             pprint(filtered_categories)
+
+#         case "update": #single report güncellenecekse id den gidilir ki karışmasın
+#             category_key = input("Category Key: ")
+#             if category_key is None:
+#                 print("There is no category.")
+#             else:
+#                 new_name = input("Please type a category name: ")
+#                 new_descp = input("Please type a description name: ")
+#                 categories.update({
+#                         category_key: {
+#                         "name": new_name,
+#                         "description": new_descp
+#                 }})
+#                 print("Category key has been edited.")
+
+#         case "delete":
+#             category_key = input("Category Key: ")
+#             is_exist = any(key== category_key for key in categories.keys())
+#             if is_exist:
+#                 del categories[category_key]
+#                 print(f"{category_key} has been removed.")
+#             else:
+#                 print("There is no such a category.")
+
+#         case _:
+#             print("Invalid process type.")
+
+#endregion
